886a2354beda54bedd4da9f60534f30cfb5a2694
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-10 08:36:29 +0530

    updated code

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
 
 const Login = () => {
+  const [signupInput, SetSignupInput] = useState({ name: "", email: "", password: "" })
+  const [loginInput, SetLoginInput] = useState({ email: "", password: "" })
+  const changeInputHandler = (e, type) => {
+    const { name, value } = e.target
+    if (type === "signup") {
+      SetSignupInput({ ...signupInput, [name]: value })
+    } else {
+      SetLoginInput({ ...loginInput, [name]: value })
+    }
+  }
+
   return (
-    <div>Login</div>
+    <div className="flex justify-center w-full items-center">
+      <Tabs defaultValue="signup" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+          <TabsTrigger value="login">Login</TabsTrigger>
+        </TabsList>
+        <TabsContent value="signup">
+          <Card>
+            <CardHeader>
+              <CardTitle>Signup</CardTitle>
+              <CardDescription>
+                Create a new account and click signup when you're done.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label>Name</Label>
+                <Input
+                  name="name"
+                  value={signupInput.name}
+                  onChange={(e) => changeInputHandler(e, "signup")}
+                  type="text"
+                  placeholder="Enter your name"
+                  required />
+              </div>
+              <div className="space-y-1">
+                <Label>Email</Label>
+                <Input
+                  name="email"
+                  value={signupInput.email}
+                  onChange={(e) => changeInputHandler(e, "signup")}
+                  type="email"
+                  placeholder="Enter your email"
+                  required/>
+              </div>
+              <div className="space-y-1">
+                <Label>Password</Label>
+                <Input name="password"
+                  value={signupInput.password}
+                  onChange={(e) => changeInputHandler(e, "signup")}
+                  type="password"
+                  placeholder="Enter your password"
+                  required />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Signup</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>
+                Login your password here. After signup , you'll be logged in.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label>Email</Label>
+                <Input
+                  name="email"
+                  value={loginInput.email}
+                  onChange={(e) => changeInputHandler(e, "login")}
+                  type="email"
+                  placeholder="Enter your email"
+                  required />
+              </div>
+              <div className="space-y-1">
+                <Label>Password</Label>
+                <Input name="password"
+                  value={loginInput.password}
+                  onChange={(e) => changeInputHandler(e, "login")}
+                  type="password"
+                  placeholder="Enter your password"
+                  required />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Login</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
   )
 }
+
 
 export default Login
