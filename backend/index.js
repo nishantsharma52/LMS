b6022f4e397dfdc22f70c1549e6c18462f253702
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-14 09:44:12 +0530

    updated code

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import connectDB from "./database/dbConnect.js";
+import userRoute from "./routes/user.route.js"
+
+dotenv.config()
+// call database connection
+connectDB()
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//default middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+  origin:"http://localhost:8080",
+  credentials:true
+}))
+
+
+//apis
+app.use("/api/v1/user",userRoute)
+
+app.listen(PORT, () => {
+  console.log(`Server listen at port ${PORT}`);
+});
+
