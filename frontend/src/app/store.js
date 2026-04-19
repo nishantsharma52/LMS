864154db23dbce81c03994eb590ad81d1fcdc38e
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-19 15:56:11 +0530

    updated code

---
+++ frontend/src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer.js";
+import { authApi } from "@/features/api/authApi.js";
+
+export const appStore = configureStore({
+    reducer: rootReducer,
+    middleware:(defaultMidddleware) => defaultMidddleware().concat(authApi.middleware)
+    
+})
+const initializeApp = async () => {
+    await appStore.dispatch(authApi.endpoints.loadUser.initiate({}, {forceRefetch:true}))
+}
+initializeApp()
