2809d35ad5172d2e0cc8472e6a508ca109428866
Nishant Sharma <nishantsharma5330@gmail.com>
2026-04-22 22:38:46 +0530

    updated code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
-import { School } from 'lucide-react'
+import { Menu, School } from 'lucide-react'
 import React from 'react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { Button } from './ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import DarkMode from '@/DarkMode';
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+
+
 
 const Navbar = () => {
+    const user = true;
     return (
         <div
             div className=' h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10 '>
@@ -12,14 +22,94 @@ const Navbar = () => {
                     <h1 className='hidden md:block font-extrabold texy-2xl'>E-Learning</h1>
                 </div>
                 {/* user icons and dark mode icon */}
-                <div>
-                    
+                <div className='flex items-center gap-8'>
+                    {
+                        user ? (
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Avatar>
+                                        <AvatarImage src="https://github.com/shadcn.png" alt="shadcn" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className="w-40" align="start">
+                                    <DropdownMenuGroup>
+                                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                                        <DropdownMenuItem>
+                                            My learning
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            Edit Profile
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            Log out
+                                        </DropdownMenuItem>
+                                    </DropdownMenuGroup>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuGroup>
+
+                                        <DropdownMenuItem>
+                                            Dashboard
+                                        </DropdownMenuItem>
+                                    </DropdownMenuGroup>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        ) : (
+                            <div className='flex items-center gap-2'>
+                                <Button variant="outline">Login</Button>
+                                <Button >Signup</Button>
+                            </div>
+                        )
+                    }
+                    <DarkMode/>
                 </div>
 
             </div>
-
+            <div>
+                {/* mobile device  */}
+                <MobileNavbar/>
+            </div>
         </div>
     )
 }
 
 export default Navbar
+
+const MobileNavbar = ()=>{
+    return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size='icon' className="rounded-full bg-gray-200 hover:bg-gray-200" variant="outline">
+            <Menu/>
+            </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+          <div className="grid gap-3">
+            <Label htmlFor="sheet-demo-name">Name</Label>
+            <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="sheet-demo-username">Username</Label>
+            <Input id="sheet-demo-username" defaultValue="@peduarte" />
+          </div>
+        </div>
+        <SheetFooter>
+          <Button type="submit">Save changes</Button>
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+ 
+
+
+    )
+}
