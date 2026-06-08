732fc0f70fdae43aa90a89e706aeb3b829ee6852
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-08 13:08:06 +0530

    updated code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,8 +13,8 @@ import { Input } from './ui/input';
 const Navbar = () => {
     const user = true;
     return (
-        <div
-            div className=' h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10 '>
+        <div  className=' h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10 '>
+         
             {/* desktop */}
             <div className=' max-w-7xl mx-auto hidden md:flex justify-between items-center h-full gap-10'>
                 <div className='flex items-center gap-2'>
