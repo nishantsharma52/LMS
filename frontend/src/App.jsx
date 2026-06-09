9e56c6d3c8ac8438e6bf8b35b286c9e34244bf48
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-09 14:42:17 +0530

    updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Login from "./pages/Login.jsx"
-function App() {
-  return (
-    <main>
-      <Navbar/>
-      <Login />
-    </main>
+import HeroSaction from "./pages/student/HeroSaction.jsx";
+import { RouterProvider } from "react-router";
+import MainLayout from "./layout/MainLayout.jsx";
 
-  )
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <HeroSaction />
+            {/* coures */}
+          </>
+            ),
+      },
+      {
+        path:"login",
+        element:<Login/>
+      }
+    ],
+  },
+]);
+function App(){
+return (
+  <main>
+    <RouterProvider router={appRouter} />
+  </main>
+);
 }
+
 
 export default App
