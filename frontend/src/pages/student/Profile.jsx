98c44cc0cfde12af5f3153792f69844e37d8da69
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-13 18:19:27 +0530

    updated code

--- frontend/src/pages/student/Profile.jsx
+++ frontend/src/pages/student/Profile.jsx
@@ -3,24 +3,56 @@ import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { Label } from '@/components/ui/label'
 import { Loader2 } from 'lucide-react'
-import React from 'react'
+import { toast } from 'sonner'
+import React, { useEffect, useState } from 'react'
 import Course from './Course'
-import { useLoadUserQuery } from '@/features/api/authApi'
+import { useLoadUserQuery, useUpdateUserMutation } from '@/features/api/authApi'
 
 const Profile = () => {
-    const {data, isLoading } = useLoadUserQuery();
-    
-    
-    if(isLoading) return <h1>Profile Loading...</h1>
-    
-    const {user} = data;
+    const [name, setName] = useState("")
+    const [profilephoto, setProfilephoto] = useState("")
+
+    const { data, isLoading, refetch } = useLoadUserQuery();
+    const [updateUser, { data: updateUserData, isLoading: updateUserIsLoading, isError, error, isSuccess }] = useUpdateUserMutation();
+
+    const onChangeHandler = (e) => {
+        const file = e.target.files?.[0];
+        if (file) setProfilephoto(file)
+    }
+
+
+    const user = data?.user;
+
+    const updateUserHandler = async () => {
+        const formData = new FormData();
+        formData.append("name", name);
+        formData.append("updateProfile", profilephoto)
+        await updateUser(formData)
+    }
+
+    useEffect(()=>{
+        refetch();
+    },[])
+    useEffect(() => {
+        if (isSuccess) {
+            refetch();
+            toast.success(data.message || "Profile Updated.")
+        }
+        if (isError) {
+            toast.error(error.message || "Failed to update profile")
+
+        }
+
+    }, [error, updateUserData, isSuccess, isError])
+
+    if (isLoading) return <h1>Profile Loading...</h1>
     return (
         <div className='my-24 px-4 mx-auto max-w-4xl'>
             <h1 className='font-bold text-2xl text-center md:text-left'>PROFILE</h1>
             <div className='flex flex-col md:flex-row items-center md:items-start gap-8 my-5'>
                 <div className='flex flex-col items-center'>
                     <Avatar className="h-24 w-24 md:h-32 md:w-32 mb-4">
-                        <AvatarImage src={user.photoUrl || "https://github.com/shadcn.png"} alt="shadcn" />
+                        <AvatarImage src={user?.photoUrl || "https://github.com/shadcn.png"} alt="shadcn" />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
                 </div>
@@ -28,19 +60,19 @@ const Profile = () => {
                     <div className='mb-2'>
                         <h1 className='font-semibold text-gray-900 dark: text-gray-100'>
                             Name:
-                            <span className='font-normal text-gray-700 dark: text-gray-300 ml-2'>{user.name}</span>
+                            <span className='font-normal text-gray-700 dark: text-gray-300 ml-2'>{user?.name}</span>
                         </h1>
                     </div>
                     <div className='mb-2'>
                         <h1 className='font-semibold text-gray-900 dark: text-gray-100'>
                             Email:
-                            <span className='font-normal text-gray-700 dark: text-gray-300 ml-2'>{user.email}</span>
+                            <span className='font-normal text-gray-700 dark: text-gray-300 ml-2'>{user?.email}</span>
                         </h1>
                     </div>
                     <div className='mb-2'>
                         <h1 className='font-semibold text-gray-900 dark: text-gray-100'>
                             Role:
-                            <span className='font-normal text-gray-700 dark: text-gray-300 ml-2'>{user.role.toUpperCase()}</span>
+                            <span className='font-normal text-gray-700 dark: text-gray-300 ml-2'>{user?.role.toUpperCase()}</span>
                         </h1>
                     </div>
                     <Dialog>
@@ -63,17 +95,17 @@ const Profile = () => {
                                     <Label>
                                         Name
                                     </Label>
-                                    <input type="text" placeholder='Name' className='col-span-3' />
+                                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' className='col-span-3' />
                                 </div>
                                 <div className=' grid grid-cols-4 gap-4 items-center'>
                                     <Label> Profile Photo  </Label>
-                                    <input type="file" accept='image/*' className='col-span-3' />
+                                    <input onChange={onChangeHandler} type="file" accept='image/*' className='col-span-3' />
                                 </div>
                             </div>
                             <DialogFooter>
-                                <Button disabled={isLoading}>
+                                <Button disabled={updateUserIsLoading} onClick={updateUserHandler}>
                                     {
-                                        isLoading ? (
+                                        updateUserIsLoading ? (
                                             <>
                                                 <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait
                                             </>
@@ -90,8 +122,12 @@ const Profile = () => {
                 <h1 className='font-medium text-lg '>Courses you're enrolled in</h1>
                 <div className='grid grid-cols-1 sm: grid-cols-2 md:grid-cols-3 gap-4 my-5'>
                     {
-                       user.enrolledCoures.length === 0 ? <h1>you haven't enrolled yet</h1> : (
-                           user.enrolledCoures.map((course) => <Course course={course} key={course._id} />)
+                        !user?.enrolledCourses || user.enrolledCourses.length === 0 ? (
+                            <h1>you haven't enrolled yet</h1>
+                        ) : (
+                            user.enrolledCourses.map((course) => (
+                                <Course course={course} key={course._id} />
+                            ))
                         )
                     }
                 </div>
