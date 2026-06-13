a8c5c6133fef81c2f2f1514560fac1fc143dd71c
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-13 18:18:59 +0530

    updated code

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs"
 import { generateToken } from "../utils/generateToken.js";
+import { deleteMediaFromCloudinary, uploadMedia } from "../utils/cloudinary.js";
 
 
 export const register = async (req, res) => {
@@ -132,8 +133,29 @@ export const updateProfile = async (req, res) => {
                 success: false
             })
         }
+        // extract public id of the old image from the url is it exists;
+        if (user.photoUrl) {
+            const publicId = user.photoUrl.split("/").pop().split(".")[0]; //extract public id
+            deleteMediaFromCloudinary(publicId)
+        }
 
-        const updatedData = { name, photoUrl }
+        //upload new photo
+        const cloudResponse = await uploadMedia(profilePhoto.path);
+        const photoUrl = cloudResponse.secure_url;
+
+        const updatedData = { name, photoUrl };
+        const updatedUser = await User.findByIdAndUpdate(userId, updatedData, {
+            new: true
+        }).select("-password")
+        return res.status(200).json({
+            success:true,
+            user:updatedUser,
+            message:"Profile updated successfully."
+        })
+
+
+
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({
