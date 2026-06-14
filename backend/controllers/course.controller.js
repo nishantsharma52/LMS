d783b749b235c2f77435bc716af2441edc354662
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-14 11:28:38 +0530

    updated code

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -1,7 +1,10 @@
-export const createCourse = async (req,res)=>{
+export const createCourse = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        console.log(error);
+        return res.status(500).json({
+            message: "Failed to create course"
+        })
     }
 }
