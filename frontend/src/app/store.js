dc1754dbeb219d6b02bf41c20b84915b510d2cc5
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-14 17:37:32 +0530

    update code

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer.js";
 import { authApi } from "@/features/api/authApi.js";
+import { courseApi } from "@/features/api/courseApi.js";
 
 export const appStore = configureStore({
     reducer: rootReducer,
-    middleware:(defaultMidddleware) => defaultMidddleware().concat(authApi.middleware)
+    middleware:(defaultMidddleware) => defaultMidddleware().concat(authApi.middleware, courseApi.middleware)
     
 })
 const initializeApp = async () => {
