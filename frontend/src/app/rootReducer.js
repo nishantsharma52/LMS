45c78164fa7a17b0c7a6676fc65a18b002c54c75
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-14 17:37:50 +0530

    updated code

--- frontend/src/app/rootReducer.js
+++ frontend/src/app/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import { authApi } from "@/features/api/authApi";
+import { courseApi } from "@/features/api/courseApi";
 
 const rootReducer = combineReducers({
     [authApi.reducerPath] : authApi.reducer,
-    auth:authReducer
+    [courseApi.reducerPath] : courseApi.reducer,
+    auth:authReducer,
 })
 
 export default rootReducer;
