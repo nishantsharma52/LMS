f4d94e2c4fcc8982edd89750b42006dd915f31ad
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-17 10:12:16 +0530

    updated code

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from "cors"
 import connectDB from "./database/dbConnect.js";
 import userRoute from "./routes/user.route.js"
 import courseRoute from "./routes/course.route.js";
+import mediaRoute from "./routes/media.route.js"
 
 dotenv.config()
 // call database connection
@@ -24,6 +25,7 @@ app.use(cors({
 
 
 //apis
+app.use("/api/v1/media",mediaRoute)
 app.use("/api/v1/user",userRoute)
 app.use("/api/v1/course",courseRoute)
 
