f62741d80a0cac66406eda521c5496aa2e84e7a2
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-18 15:18:13 +0530

    updated code

--- frontend/src/pages/admin/lecture/CreateLecture.jsx
+++ frontend/src/pages/admin/lecture/CreateLecture.jsx
@@ -1,6 +1,6 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button;'
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { useCreateLectureMutation, useGetCourseLectureQuery } from '@/features/api/courseApi'
 import { Loader2 } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
