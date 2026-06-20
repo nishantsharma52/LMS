ce8544bf4e35c2a66a11b194c74e1f9449fa60ca
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-20 09:37:35 +0530

    updated code

--- frontend/src/pages/admin/course/CourseTab.jsx
+++ frontend/src/pages/admin/course/CourseTab.jsx
@@ -119,7 +119,7 @@ const CourseTab = () => {
                     </CardDescription>
                 </div>
                 <div className='space-x-2'>
-                    <Button variant='outline' onClick={() => publishStatusHandler(courseByIdData?.course.isPublished ? "false" : "true")}>
+                    <Button disabled={courseByIdData?.course.lectures.length === 0   } variant='outline' onClick={() => publishStatusHandler(courseByIdData?.course.isPublished ? "false" : "true")}>
                         {courseByIdData?.course.isPublished ? "Unpublished" : "Publish"}
                     </Button>
                     <Button>
