3083779a7d8ba965cba2e34930cad3397c09c50a
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-21 10:33:33 +0530

    updated code

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -1,50 +1,50 @@
 import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
-    courseTitle:{
-        type:String,
-        required:true,
+    courseTitle: {
+        type: String,
+        required: true,
     },
-    subTitle:{
-        type:String,
+    subTitle: {
+        type: String,
     },
-    description:{
-        type:String,
+    description: {
+        type: String,
     },
-    category:{
-        type:String,
-        required:true,
+    category: {
+        type: String,
+        required: true,
     },
-    courseLevel:{
-        type:String,
-        enum:["Beginner","Medium", "Advance"]
+    courseLevel: {
+        type: String,
+        enum: ["Beginner", "Medium", "medium", "Intermediate", "Advance", "Advanced"] // Sabhi variations allow kar do taaki safe raho
     },
-    coursePrice:{
-        type:Number,
+    coursePrice: {
+        type: Number,
     },
-    courseThumbnail:{
-        type:String,
+    courseThumbnail: {
+        type: String,
     },
-    enrolledStudents:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
-        }
-    ],
-    lectures:[
+    enrolledStudents: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref:"Lecture"
+            ref: "User",
         }
     ],
-    creator:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
+    lectures: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Lecture"
+        }
+    ],
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     },
-    isPublished:{
-        type:Boolean,
-        default:false,
+    isPublished: {
+        type: Boolean,
+        default: false,
     },
-}, {timestamps:true});
+}, { timestamps: true });
 
 export const Course = mongoose.model("Course", courseSchema)
