d7d3b29e0bf00421d13d0d8ace005a5ac3ee2d98
Nishant Sharma <nishantsharma5330@gmail.com>
2026-06-27 11:18:41 +0530

    updated code

--- frontend/src/pages/student/MyLearning.jsx
+++ frontend/src/pages/student/MyLearning.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Course from './Course';
+import { useLoadUserQuery } from '@/features/api/authApi';
 
 const MyLearning = () => {
-    const isLoading = false;
-    const myLearningCourses = [];
+    
+  const {data,isLoading} = useLoadUserQuery()
+
+  const myLearning = data?.user.enrolledCourses || []
     return (
         <div className='max-w-4xl mx-auto my-24 px-4 md:px-0'>
             <h1 className='font-bold text-2xl '> MY LEARNING </h1>
@@ -11,10 +14,10 @@ const MyLearning = () => {
                 {
                     isLoading ? (
                         <MyLearningSkeleton />
-                    ) : myLearningCourses.length === 0 ? (<p> You are not enrolled in any courses</p>) :
+                    ) : myLearning.length === 0 ? (<p> You are not enrolled in any courses</p>) :
                         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
                             {
-                                [1, 2].map((course, index) => <Course key={index} course={course}/>)
+                                myLearning.map((course, index) => <Course key={index} course={course}/>)
                             }
                         </div>
 
